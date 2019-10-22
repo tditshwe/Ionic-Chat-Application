741ea9659535e7fef231099e6da6bc386b7a9ed1
Kgothatso Ditshwene <kgothatsod@bcx.co.za>
2019-10-22 17:01:01 +0200

    Participants on group info

--- src/pages/group-info/group-info.ts
+++ src/pages/group-info/group-info.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import {NavController, NavParams } from 'ionic-angular';
+import {AppProvider} from '../../providers/app/app';
 
 @Component({
   selector: 'page-group-info',
   templateUrl: 'group-info.html',
 })
 export class GroupInfoPage {
+  participants: any;
+  group: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public appProv: AppProvider) {
+    this.appProv.showLoading('Waiting for participants...');
+    this.group = this.appProv.currentGroup;
+
+    this.appProv.getData<any>('group/' + this.group.id).subscribe(res => {
+      this.participants = res;
+      this.appProv.loading.dismiss();
+    }, err => {
+      alert(err.console.error());
+    });
   }
 
   ionViewDidLoad() {
