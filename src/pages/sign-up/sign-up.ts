16d79c9cf89a3b36d6e81549bf67c442d02daffe
Kgothatso Ditshwene <kgothatsod@bcx.co.za>
2019-11-22 16:41:24 +0200

    Working on password validation

--- src/pages/sign-up/sign-up.ts
+++ src/pages/sign-up/sign-up.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { PasswordValidator } from '../../validators/password.validator';
 
 import { LoginPage } from '../login/login';
 import {AppProvider} from '../../providers/app/app';
@@ -16,12 +18,35 @@ export class SignUpPage {
   passConfirm: String;
   name: String;
 
+  sign_up_form: FormGroup;
+  passwords_group: FormGroup;
+
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public alertCtrl:AlertController,
               public http: HttpClient,
-              public appProv: AppProvider) {
+              public appProv: AppProvider,
+              public formBuilder: FormBuilder) {
     // If we navigated to this page, we will have an item available as a nav param
+  }
+
+  ionViewWillLoad() {
+    this.sign_up_form = this.formBuilder.group({
+      name: new FormControl('', Validators.required),
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+      password_confirm: new FormControl('', Validators.required),
+    });
+
+    this.passwords_group = new FormGroup({
+      password: new FormControl('', Validators.compose([
+        Validators.minLength(6),
+        Validators.required
+      ])),
+      confirm_password: new FormControl('', Validators.required)
+    }, (formGroup: FormGroup) => {
+      return PasswordValidator.areEqual(formGroup);
+    });
   }
 
   signUp(event, item) {
